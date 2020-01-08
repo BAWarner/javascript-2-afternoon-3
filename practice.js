fdc8d3d643ddd76c58d74b8e5c22b0edb0168cc7
Brandon Warner <warner.brandon.a@gmail.com>
2020-01-08 16:24:12 -0600

    Finish assignemnt

--- practice.js
+++ practice.js
@@ -117,6 +117,10 @@ contains(names, 'Colt', function(result){
   Remove any duplicate values from the array, and invoke the callback with the modified array as an argument.
 */
 
+function uniq(arr, callback){
+  let filtered = [... new Set(arr)];
+  callback(filtered);
+}
 
 // Do not edit the code below.
 uniq(names, function(uniqArr){
@@ -133,7 +137,11 @@ uniq(names, function(uniqArr){
   For each name in the array, invoke the callback and pass in the name and the name's index as arguments.
 */
 
-//Code Here 
+function each(arr, callback){
+  for(let i = 0; i < arr.length; i++){
+    callback(arr[i], i);
+  }
+}
 
 // Do not edit the code below.
 each(names, function(item, indice){
@@ -150,7 +158,13 @@ each(names, function(item, indice){
   When the correct user object is found, invoke the callback with the user object as an argument.
 */
 
-// Code here
+function getUserById(arr, id, callback){
+  for(let i = 0; i < arr.length; i++){
+    if(arr[i].id == id){
+      callback(arr[i]);
+    }
+  }
+}
 
 // Do not edit the code below.
 var users = [
